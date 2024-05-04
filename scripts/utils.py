770b77a5b38b51eedca6b8dcd8951c0a07554881
DoduCode <itzrealdodu@gmail.com>
2024-05-04 22:06:21 +0530

    Created the circle collision function

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 import pygame
 
@@ -17,3 +18,14 @@ def load_images(path):
     for img in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(f"{path}/{img}"))
     return images
+
+def circle_collision(radius_1, radius_2, pos_1, pos_2):
+    s1 = abs(pos_1[0] - pos_2[0])
+    s2 = abs(pos_1[1] - pos_2[1])
+    hy = math.sqrt((s1 ** 2) + ( s2 ** 2))
+
+    if hy < (radius_1 + radius_2):
+        return True
+    
+    else:
+        return False
